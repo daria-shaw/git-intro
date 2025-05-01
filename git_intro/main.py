15d2aabb54d0b188836a24b590dade44a2644e02
Daria Shaw <me@daria.sh>
2025-05-01 18:10:27 +0100

    add broken test

---
+++ git_intro/main.py
@@ -0,0 +1,11 @@
+def hello_world_msg() -> str:
+    return "hello world"
+
+
+def main() -> None:
+    msg = hello_world_msg()
+    print(msg)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from git_intro.main import hello_world_msg
+
+
+def test_hello_world_msg_returns_expected_message() -> None:
+    assert hello_world_msg() == "hello"
