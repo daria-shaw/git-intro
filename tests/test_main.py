2a178b0270511209df29ffe0b399956160ae843b
LeahGreenawayRI <leah.greenaway@rentokil-initial.com>
2025-05-02 10:26:44 +0000

    Fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,4 +2,4 @@ from git_intro.main import hello_world_msg
 
 
 def test_hello_world_msg_returns_expected_message() -> None:
-    assert hello_world_msg() == "hello"
+    assert hello_world_msg() == "hello world"
